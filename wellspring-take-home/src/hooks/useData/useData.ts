1c2ec1f24ad1f1975fe8d55eebf3fb49c86ef96b
jpbullalayao <jpbullalayao@gmail.com>
2023-09-08 22:12:27 -0700

    Add error handling for API calls

--- wellspring-take-home/src/hooks/useData/useData.ts
+++ wellspring-take-home/src/hooks/useData/useData.ts
@@ -6,8 +6,17 @@ export const useData = (url: string) => {
   const [data, setData] = useState([]);
 
   const fetchData = async (url: string) => {
-    const response = await axios.get(url);
-    return response.data;
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      // In a production environment you'd typically want to
+      // raise this error in some JS monitoring application such
+      // as Sentry or Rollbar.  For the purposes of this project,
+      // we will just console.error the error object.
+      console.error(error);
+      return [];
+    }
   };
 
   useEffect(() => {
